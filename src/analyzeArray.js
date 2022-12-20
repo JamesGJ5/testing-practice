27645a31974086f3b176bd920e8762de026cc712
James Graca-Jones <jamesgracajones@gmail.com>
2022-12-20 20:46:27 +0000

    Create constructor for AnalyzeArray objects with length class

---
+++ src/analyzeArray.js
@@ -0,0 +1,9 @@
+export default class AnalyzeArray {
+  constructor(array) {
+    this.array = array;
+  }
+
+  length() {
+    return this.array.length;
+  }
+}
---
+++ src/tests/analyzeArray.test.js
@@ -0,0 +1,25 @@
+import AnalyzeArray from '../analyzeArray';
+
+describe('length', () => {
+  test('Returns length of non-empty array', () => {
+    expect(new AnalyzeArray([1, 6, 3]).length()).toBe(3);
+  });
+
+  test('Returns length of empty array', () => {
+    expect(new AnalyzeArray([]).length()).toBe(0);
+  });
+});
+
+// describe('average', () => {
+//   test('Returns 5 for [1, 6, 3]', () => {
+//     expect(new AnalyzeArray([1, 6, 3]).average()).toBe(5);
+//   });
+
+//   test('Does fairly well for floating point numbers', () => {
+//     expect(new AnalyzeArray([1.1, 2.2, 3.3]).average()).toBeCloseTo(2.2);
+//   });
+
+//   test('Returns null for empty arrays', () => {
+//     expect(new AnalyzeArray([]).average()).toBeNull();
+//   });
+// });
