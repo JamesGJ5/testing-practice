f8bb64dfb967aaf50cf881fb5f3d992f8c115eaf
James Graca-Jones <jamesgracajones@gmail.com>
2022-12-20 19:30:48 +0000

    Write a subtract method into the Calculator module

--- src/calculator.js
+++ src/calculator.js
@@ -2,4 +2,8 @@ export default class Calculator {
   static add(num1, num2) {
     return num1 + num2;
   }
+
+  static subtract(num1, num2) {
+    return this.add(num1, -num2);
+  }
 }
--- src/tests/calculator.test.js
+++ src/tests/calculator.test.js
@@ -9,3 +9,13 @@ describe('add', () => {
     expect(Calculator.add(1.31, 89.12)).toBeCloseTo(90.43);
   });
 });
+
+describe('subtract', () => {
+  test('Subtracts 2 from 1 to make -1', () => {
+    expect(Calculator.subtract(1, 2)).toBe(-1);
+  });
+
+  test('Subtracts floating point numbers fairly well', () => {
+    expect(Calculator.subtract(1.31, 89.12)).toBeCloseTo(-87.81);
+  });
+});
